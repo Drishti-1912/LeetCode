1ff2691290342464566d7900388ff3d949259e18
Drishti-1912 <114720114+Drishti-1912@users.noreply.github.com>
2025-02-15 16:51:59 +0530

    Time: 0 ms (100%), Space: 29.6 MB (59.56%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -3,21 +3,9 @@ public:
     void rotate(vector<int>& nums, int k) {
         int n=nums.size();
         k=k%n;
-        if( k==0 || n==1 ) return;
-            //temp
-            // for(int i=n-k; i<n; i++){
-            //     temp.push_back(nums[i]);
-            // }
-            vector<int> temp(nums.end() - k, nums.end());
-
-            //shifting
-            for(int i=n-1;i>=k;i--){
-                nums[i]=nums[i-k];
-            }
-            //put back temp
-            for(int i=0;i<k;i++){
-                nums[i]=temp[i];
-            }
-        }
-    
+        if(n==1 || k==0) return;
+        reverse(nums.begin(),nums.begin()+n-k);
+        reverse(nums.begin()+n-k,nums.end());
+        reverse(nums.begin(),nums.end());
+    }
 };
