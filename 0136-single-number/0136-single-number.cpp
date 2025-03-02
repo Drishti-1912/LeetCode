c2bc36f5dc54e17977190a76437d64340172d8da
Drishti-1912 <114720114+Drishti-1912@users.noreply.github.com>
2025-03-02 23:29:10 +0530

    Time: 0 ms (100%), Space: 20.6 MB (83.32%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        unordered_map<int,int> a;
-	   for(auto x: nums)
-		   a[x]++;
-	   for(auto z:a)
-		   if(z.second==1)
-			   return z.first;
-	   return -1;
+       int xorr=0;
+       for(int i=0;i<nums.size();i++){
+        xorr=xorr^nums[i];
+       }
+       return xorr;
     }
 };
