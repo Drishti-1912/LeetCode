1eca9d351a4cdf072e8439a750871f17be5d0934
Drishti-1912 <114720114+Drishti-1912@users.noreply.github.com>
2025-05-26 11:04:54 +0530

    Time: 0 ms (100%), Space: 14.7 MB (50.59%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        for(int i=0;i<nums.size();i++){
-            for(int j=i+1;j<nums.size();j++){
-                // if(nums[j]==target-nums[i]) return {i,j};
-                if (target==nums[i]+nums[j]) return {i,j};
-            }
+        unordered_map<int,int> mpp;
+       for(int i=0;i<nums.size();i++){
+        int curr=nums[i];
+        int needed=target-curr;
+
+        if (mpp.find(needed) != mpp.end()){ //“If the number we need to complete the target sum is already in the map, then return YES because a valid pair exists.”
+        return {mpp[needed],i};
+       }
+       mpp[curr]=i; //store current number in the map
         }
-    return {};
+    return {-1,-1};
     }
 };
