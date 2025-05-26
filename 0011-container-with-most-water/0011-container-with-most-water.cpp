0905e84e6923d626875ae59466572d6bcc79bee6
Drishti-1912 <114720114+Drishti-1912@users.noreply.github.com>
2025-05-26 12:07:18 +0530

    Time: 0 ms (100%), Space: 62.7 MB (97.1%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     int maxArea(vector<int>& height) {
         int maxArea=0;
-        int i=0,j=height.size()-1;
+        int i=0,j=height.size()-1; //Two pointer
         while(i<j){
-            int currentArea=min(height[i],height[j])*(j-i);
+            int currentArea=min(height[i],height[j]) * (j-i);
             maxArea=max(maxArea,currentArea);
             if(height[i]<height[j]) i++;
             else j--;
