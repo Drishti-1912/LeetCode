448c1bc36388c70674449a53a04539a3a5004267
Drishti-1912 <114720114+Drishti-1912@users.noreply.github.com>
2025-06-07 13:15:34 +0530

    Time: 0 ms (100%), Space: 7.7 MB (86.62%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -1,18 +1,25 @@
-class Solution {
+
+   class Solution {
 public:
+    int getNext(int n) {
+        int sum = 0;
+        while (n != 0) {
+            int digit = n % 10;
+            sum += digit * digit;
+            n = n / 10;
+        }
+        return sum;
+    }
+
     bool isHappy(int n) {
-        unordered_set <int> usedIntegers;
-        while(true){
-        int sum=0;
-        while(n!=0){
-            int digit=n%10;
-            sum+=digit*digit;
-            n=n/10;
+        int slow = n;
+        int fast = getNext(n);
+
+        while (fast != 1 && slow != fast) {
+            slow = getNext(slow);
+            fast = getNext(getNext(fast));
         }
-        if(sum==1)return true;
-        if(usedIntegers.count(sum)) return false;
-        usedIntegers.insert(sum);
-        n=sum;
-        }
+
+        return fast == 1;
     }
 };
