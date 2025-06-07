01676e7dcdc215ca70d8ff53b3166d2fbe38d504
Drishti-1912 <114720114+Drishti-1912@users.noreply.github.com>
2025-06-07 13:06:11 +0530

    Time: 0 ms (100%), Space: 8.3 MB (40.05%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     bool isHappy(int n) {
-        unordered_set<int> usedInteger;
+        unordered_set <int> usedIntegers;
         while(true){
-            int sum=0;
-            int temp=n; //to extract digits from temp, preserving n
-            while(temp!=0){
-                int digit=temp%10;
-                sum+= digit*digit;
-                temp = temp/10;
-            }
-            if (sum==1) return true;
-
-            if(usedInteger.count(sum)) return false;
-            usedInteger.insert(sum);
-            n=sum;
+        int sum=0;
+        while(n!=0){
+            int digit=n%10;
+            sum+=digit*digit;
+            n=n/10;
+        }
+        if(sum==1)return true;
+        if(usedIntegers.count(sum)) return false;
+        usedIntegers.insert(sum);
+        n=sum;
         }
     }
 };
