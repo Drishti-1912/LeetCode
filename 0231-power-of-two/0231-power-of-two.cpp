169b0e0a34c67266419a81e30f300145260cebb6
Drishti-1912 <114720114+Drishti-1912@users.noreply.github.com>
2025-06-09 17:17:24 +0530

    Time: 0 ms (100%), Space: 7.8 MB (53.19%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -3,7 +3,7 @@ public:
     bool isPowerOfTwo(int n) {
         int x;
         if(n<=0) return false;
-        for(int x=0;x<31;x++){
+        for(int x=0;x<=31;x++){
         if(n==pow(2,x)) return true;
         }
         return false;
