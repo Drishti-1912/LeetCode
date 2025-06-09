92cf0ceec8cfd766046a8bd74c1cafe2af111544
Drishti-1912 <114720114+Drishti-1912@users.noreply.github.com>
2025-06-09 14:26:45 +0530

    Time: 0 ms (100%), Space: 20.5 MB (6.84%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.cpp
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.cpp
@@ -1,31 +1,16 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* removeElements(ListNode* head, int val) {
-        ListNode* dummy=new ListNode(0);
-        dummy->next=head;
-        ListNode* curr=dummy;
+        // Base case: if the list is empty, return null
+        if (head == nullptr) return nullptr;
 
-        // Traverse the list while current node and its next exist
-        while (curr != nullptr && curr->next != nullptr) {
-            // If the next node's value is equal to the target, skip it
-            if (curr->next->val == val) {
-                curr->next = curr->next->next;  // Remove the node
-            } else {
-                curr = curr->next;  // Move to next node
-            }
-        }
+        // Recursively call on the rest of the list
+        head->next = removeElements(head->next, val);
 
-        // Return the new head (skipping dummy)
-        return dummy->next;
+        // If current node matches the target value, skip it
+        if (head->val == val)
+            return head->next;
+        else
+            return head;
     }
 };
