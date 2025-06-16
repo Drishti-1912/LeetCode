94d48ffca3d2bbe6ce75301e0cfbbde924453be6
Drishti-1912 <114720114+Drishti-1912@users.noreply.github.com>
2025-06-16 14:17:09 +0530

    Time: 2 ms (87.52%), Space: 17.4 MB (11.13%) - LeetHub

--- 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.cpp
+++ 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
     long long minimumSteps(string s) {
-        int i=0;
-        long long cnt=0;
-        for(int j=0;j<s.size();j++){
-            if (s[j]=='0'){
-                swap(s[i],s[j]);
-                cnt+=j-i;
-                i++;
+        long long steps = 0;
+        long long zeroCount = 0;
+
+        for (int i = s.size() - 1; i >= 0; i--) {
+            if (s[i] == '0') {
+                zeroCount++;  // zeros to the right of current position
+            } else {
+                steps += zeroCount;  // each '1' must move past all zeros on the right
             }
         }
-    return cnt;
+
+        return steps;
     }
 };
+
