3fde26a230388c8bcf92f49698e27988b46a8cda
Drishti Malhotra <114720114+Drishti-1912@users.noreply.github.com>
2025-09-11 19:07:57 +0530

    Time: 0 ms (100%), Space: 23.9 MB (55.36%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,13 +1,20 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int left=0;
-        int right=0;
-        if(right==-1) return; //no non zeroes
-        for(int right=0;right<nums.size();right++){
-            if(nums[right]!=0){
-                swap(nums[left],nums[right]);
-                left++;
+        //find first non zero number
+        int j=-1; 
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==0){
+                j=i;
+                break;
+            }
+        }
+        if(j==-1) return; //no non zeroes
+        
+        for(int i=j+1;i<nums.size();i++){
+            if(nums[i]!=0){
+                swap(nums[i],nums[j]);
+                j++;
             }
         }
     }
