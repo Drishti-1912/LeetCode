42d97e4c3b986cd10f3aac6707eba21f20f9795a
Drishti Malhotra <114720114+Drishti-1912@users.noreply.github.com>
2025-09-24 00:03:58 +0530

    Time: 0 ms (100%), Space: 28.1 MB (86.8%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,18 +1,19 @@
-class Solution {   //boyre moore's algorithm
+class Solution {
 public:
- int majorityElement(vector<int>& nums) {
-    int count=0;
-    int cand=0;
-    for(int i=0;i<nums.size();i++){
-        if(count==0){
-            cand=nums[i];
+    int majorityElement(vector<int>& nums) {
+        int n=nums.size();
+        int cnt=0;
+        int el;
+        for(int i=0;i<n;i++){
+            if(cnt==0){
+                cnt=1;
+                el=nums[i];
+            }else if(nums[i]==el){
+                cnt++;
+            }else{
+                cnt--;
+            }
         }
-        if(cand==nums[i]){
-            count++;
-        }else{
-            count--;
-        }
+    return el;
     }
-return cand;
-}
 };
