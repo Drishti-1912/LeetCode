06b68a5f5677a3327d04e2ec74a963d9ab790e4a
Drishti Malhotra <114720114+Drishti-1912@users.noreply.github.com>
2025-09-23 19:33:08 +0530

    Time: 0 ms (100%), Space: 11.7 MB (49.11%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,14 +1,24 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        map<int,int> freq;
-        for(int x:nums){
-            freq[x]++;
+        int z=0,o=0,t=0;
+        int n=nums.size();
+        for(int i:nums){
+            if(i==0) z++;
+            else if (i==1) o++;
+            else t++;
         }
-        int idx=0;
-        for(auto &it:freq){
-            for(int i=0;i<it.second;i++){
-                nums[idx++] = it.first;
+        for(int i=0;i<n;i++){
+            if(z){
+                nums[i]=0;
+                z--;
+            }
+            else if(o){
+                nums[i]=1;
+                o--;
+            }
+            else {
+                nums[i]=2;
             }
         }
     }
