204eb0f3542c315b28100480a39d004df4f72bf2
Drishti Malhotra <114720114+Drishti-1912@users.noreply.github.com>
2025-10-02 00:30:57 +0530

    Time: 0 ms (100%), Space: 8 MB (11.04%) - LeetHub

--- 1642-water-bottles/1642-water-bottles.cpp
+++ 1642-water-bottles/1642-water-bottles.cpp
@@ -3,16 +3,19 @@ public:
     int numWaterBottles(int numBottles, int numExchange) {
         int consumedBottles = 0;
 
-        while (numBottles >= numExchange) {
-            // Consume numExchange full bottles.
+        while (numBottles >= numExchange) {  //as long as you have enough bottles to exchange.
+
+            /*You drink numExchange bottles in this round.
+            Add that to your total count of consumed bottles. */
             consumedBottles += numExchange;
-            numBottles -= numExchange;
+
+            numBottles -= numExchange; //Reduce the number of full bottles because you just drank them
 
             // Exchange them for one full bottle.
             numBottles++;
         }
 
-        // Consume the remaining numBottles (<numExchange).
+        // Total bottles drunk = bottles you drank during exchanges + the remaining bottles.
         return consumedBottles + numBottles;
     }
 };
