61c838a8646c3160a7365cbc68b249317b0e6266
Drishti Malhotra <114720114+Drishti-1912@users.noreply.github.com>
2025-10-28 15:36:36 +0530

    Time: 479 ms (47.33%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,5 +1,6 @@
 # Write your MySQL query statement below
 SELECT today.id 
 FROM Weather yesterday
-CROSS JOIN Weather today
+ JOIN Weather today
 WHERE DATEDIFF(today.recordDate,yesterday.recordDate)=1 AND today.temperature>yesterday.temperature
+-- DATEDIFF() function is used to check if they are consecutive days (with a difference of 1 day).
